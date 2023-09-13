185c021a0d4dc67761110d2d73558fe9df36866d
RelampagoAzul5 <giovannihpf@gmail.com>
2023-09-13 10:13:38 -0300

    criando uma conferencia da base de dados para o setup de todas as paginas do site

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -2,16 +2,22 @@ from django.contrib import admin
 from site_setup.models import MenuLink, SiteSetup
 
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path',
-    list_display_links = 'id', 'text', 'url_or_path',
-    search_fields = 'id', 'text', 'url_or_path',
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display = 'id', 'text', 'url_or_path',
+#     list_display_links = 'id', 'text', 'url_or_path',
+#     search_fields = 'id', 'text', 'url_or_path',
+
+
+class MenuLinkInLine(admin.StackedInline):
+    model = MenuLink
+    extra = 1
 
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = 'title', 'description',
+    inlines = MenuLinkInLine,
 
     def has_add_permission(self, request):
         return not SiteSetup.objects.exists()
---
+++ djangoapp/site_setup/context_processors.py
@@ -0,0 +1,8 @@
+from site_setup.models import SiteSetup
+
+
+def site_setup(request):
+    setup = SiteSetup.objects.order_by('-id').first()
+    return {
+        'site_setup': setup
+    }
