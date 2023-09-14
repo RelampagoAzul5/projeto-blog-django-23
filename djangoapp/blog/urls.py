ca0a13fc59580e4d541a0ea4bab238ed0299b823
RelampagoAzul5 <giovannihpf@gmail.com>
2023-09-14 20:48:37 -0300

    criando o Layout HTML e CSS do blog

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from blog.views import index
+from blog.views import index, post, page
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('post/', post, name='post'),
+    path('page/', page, name='page'),
 ]
