a8555393cb4c26fd8194d85d83b04c117b9869f3
RelampagoAzul5 <giovannihpf@gmail.com>
2023-10-16 14:16:53 -0300

    Começando a fazer a parte interna do post

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from blog.views import index, post, page
+from blog.views import index, post, page, created_by, category
 app_name = 'blog'
 
 urlpatterns = [
     path('', index, name='index'),
     path('post/<slug:slug>/', post, name='post'),
-    path('page/', page, name='page'),
+    path('page/<slug:slug>/', page, name='page'),
+    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
 ]
