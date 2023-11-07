df5c4a52f17373b09df82a3429b9fe43553a01a6
RelampagoAzul5 <giovannihpf@gmail.com>
2023-11-06 22:15:43 -0300

    Migrando a view category para ListView e usando allow_empty para 404

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from blog.views import (PostListView, post, page,
-                        CreatedByListView, category, tag, search)
+from blog.views import (PostListView, CreatedByListView, CategoryListView, post, page,
+                         tag, search)
 app_name = 'blog'
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -129,6 +129,20 @@ def category(request, slug):
             'page_title': 'Home',
         }
     )
+class CategoryListView(PostListView):
+    allow_empty = False
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(
+            category__slug=self.kwargs.get('slug')
+        )
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+    
+        page_title = f'{self.object_list[0].category.name} - Categoria' # type:ignore
+        ctx.update({
+            'page_title': page_title
+        })
+        return ctx
 
 
 def tag(request, slug):
